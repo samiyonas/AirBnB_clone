09e0f4f954758793409cd76716fe5d7d7a374482
samiyonas <samuel.x.yonas@gmail.com>
2024-03-24 08:17:41 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -25,19 +25,17 @@ class BaseModel:
         """
         if len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key == 'id':
-                    self.id = kwargs[key]
-                elif key == 'created_at':
-                    self.created_at = datetime.fromisoformat(kwargs[key])
-                elif key == 'updated_at':
-                    self.updated_at = datetime.fromisoformat(kwargs[key])
-                elif key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid1())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        models.storage.new(self)
 
     def __str__(self):
         """ Returns:
